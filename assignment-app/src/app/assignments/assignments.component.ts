27507e4311d41bb3e0e7fd3d38e4519ba246bc6c
FiaritiaRahamison <161418627+FiaritiaRahamison@users.noreply.github.com>
2024-02-27 14:02:02 +0300

    Ajout d'un objet par un formulaire.

--- assignment-app/src/app/assignments/assignments.component.ts
+++ assignment-app/src/app/assignments/assignments.component.ts
@@ -1,30 +1,68 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+
 import { RenduDirective } from '../shared/rendu.directive';
+import { Assignment } from './assignment.model';
+
 @Component({
   selector: 'app-assignments',
   standalone: true,
-  imports: [CommonModule, RenduDirective],
+  imports: [
+    CommonModule,
+    RenduDirective,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './assignments.component.html',
-  styleUrl: './assignments.component.css'
+  styleUrl: './assignments.component.css',
+  providers: [provideNativeDateAdapter()],
 })
 export class AssignmentsComponent {
   titre = "Voici mon app sur les devoirs";
-  assignments = [
+  assignments: Assignment[] = [
     {
       nom: "Devoir 1",
-      dateRendu: "2024-02-15",
+      dateRendu: new Date("2024-02-15"),
       estRendu: false
     },
     {
       nom: "Devoir 2",
-      dateRendu: "2024-02-20",
+      dateRendu: new Date("2024-02-20"),
       estRendu: false
     },
     {
       nom: "Devoir 3",
-      dateRendu: "2024-02-24",
+      dateRendu: new Date("2024-02-24"),
       estRendu: true
     }
-  ]
+  ];
+  nomAssignment: string = "";
+  dateRendu = undefined;
+
+  onSubmit(event: any) {
+    if ((this.nomAssignment == '') || (this.dateRendu === undefined)) return;
+
+    console.log("NOM ASSIGNMENT = " + this.nomAssignment);
+    console.log("DATE RENDU = " + this.dateRendu);
+
+    const newAssignment = {
+      nom: this.nomAssignment,
+      estRendu: false,
+      dateRendu: this.dateRendu
+    };
+
+    console.log(newAssignment)
+    this.assignments.push(newAssignment);
+  }
 }
+
+
